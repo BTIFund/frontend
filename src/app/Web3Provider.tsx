715172e41e71632e68d3e8dd8edbacc584a066a9
AdityaWisnuDev <wisnu.barata111@gmail.com>
2025-05-09 06:54:16 +0700

    feat: init xellar provider

---
+++ src/app/Web3Provider.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Config, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { XellarKitProvider, defaultConfig, darkTheme } from "@xellar/kit";
+import { polygonAmoy } from "viem/chains";
+
+const walletConnectProjectId = "YOUR_WALLET_CONNECT_PROJECT_ID";
+const xellarAppId = "378510d2-ba41-4037-a562-86b24d668baf";
+
+const config = defaultConfig({
+  appName: "Xellar",
+  walletConnectProjectId,
+  xellarAppId,
+  xellarEnv: "sandbox",
+  chains: [polygonAmoy],
+}) as Config;
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <XellarKitProvider theme={darkTheme}>{children}</XellarKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
