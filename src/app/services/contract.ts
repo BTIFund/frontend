35857a0ca9bcb8dd2e9fb725c22e0bb219d6c3e8
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-05-11 11:41:59 +0700

    chore: add idrx abi

--- src/app/services/contract.ts
+++ src/app/services/contract.ts
@@ -1,4 +1,4 @@
-const SolarContract = "0x2EaD3dD41578abD1caFaBC66700aDf54C86dbe66";
-const IDRCContract = "0xD63029C1a3dA68b51c67c6D1DeC3DEe50D681661";
+const BTIContract = "0x2EaD3dD41578abD1caFaBC66700aDf54C86dbe66";
+const IDRXContract = "0xD63029C1a3dA68b51c67c6D1DeC3DEe50D681661";
 
-export { SolarContract, IDRCContract };
+export { BTIContract, IDRXContract };
