dce60e8aad07f057c676ef4c05ce237cf3985dd5
Arjuna Marcelino <arjunamarcelino@gmail.com>
2025-05-17 17:19:56 +0700

    feat: change address

--- src/app/services/contractAddress.ts
+++ src/app/services/contractAddress.ts
@@ -1,4 +1,4 @@
-const SolarContract = "0x48f20838AcC9b8bb13A02262282D7acCADb20b79";
+const SolarContract = "0xa9e536C01D3e4CaDa6Ad39f07F12dA9F6F22e523";
 const IDRCContract = "0xD63029C1a3dA68b51c67c6D1DeC3DEe50D681661";
 
 export { SolarContract, IDRCContract };
