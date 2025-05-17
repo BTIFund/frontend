f86a5b66e71e854a95ff10cc952caf70cf4317c2
Arjuna Marcelino <arjunamarcelino@gmail.com>
2025-05-17 22:39:27 +0700

    fix: build error

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -5,6 +5,14 @@ import {
 } from "../(pages)/dashboard/types/transaction.types";
 import { Project } from "../(pages)/dashboard/types/project.types";
 
+function formatAmount(value: number | string): string {
+  const stringValue = value.toString().padStart(3, "0");
+  const intPart = stringValue.slice(0, -2);
+  const decimalPart = stringValue.slice(-2);
+  const formattedInt = Number(intPart).toLocaleString("id-ID");
+  return `${formattedInt},${decimalPart}`;
+}
+
 export function mapTransactionsToDisplay(
   raw: Transaction[],
   projects: Project[]
@@ -17,7 +25,7 @@ export function mapTransactionsToDisplay(
       id: index + 1,
       type: tx.transactionType,
       project: projectName,
-      amount: Number(tx.amount),
+      amount: formatAmount(Number(tx.amount)),
       date: format(Number(tx.timestamp) * 1000, "dd MMM yyyy"),
       status: "In Progress",
     };
